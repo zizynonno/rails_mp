28c42a10f3b172db843252525ef147767da4080c
zizynonno <zizynonno@gmail.com>
2020-06-29 02:21:25 +0900

    add: home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す。
+  def full_title(page_title='')
+    base_title = 'Rails_MP'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -31,7 +31,7 @@ RSpec.feature 'タイトル' do
   end
   scenario 'タイトル' do
     visit '/static_pages/home'
-    expect(page).to have_title "Home | #{@base_title}"
+    expect(page).to have_title "#{@base_title}"
     visit '/static_pages/help'
     expect(page).to have_title("Help | #{@base_title}", exact: true)
     visit '/static_pages/about'
