870340474fe39c9f17aaf25136d33d77ced4945b
zizynonno <zizynonno@gmail.com>
2020-07-02 00:11:21 +0900

    create: user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  #FactoryBotでユーザー情報を@userに登録
+  before do
+    @user = create(:user)
+  end
+
+  describe 'バリデーション' do
+    it 'nameとemailどちらも値が設定されていれば、OK' do
+      expect(@user.valid?).to eq(true)
+    end
+
+    it 'nameが空だとNG' do
+      @user.name = ''
+      expect(@user.valid?).to eq(false)
+    end
+
+    it 'emailが空だとNG' do
+      @user.email = ''
+      expect(@user.valid?).to eq(false)
+    end
+  end
+
+ 
+end
