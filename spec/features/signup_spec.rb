ea93f9da038c81f17b14106612cc62f1412fd127
zizynonno <zizynonno@gmail.com>
2020-07-05 00:36:00 +0900

    add: new

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign up' do
+  scenario "ユーザー登録を行う" do
+    visit root_path
+    expect(page).to have_http_status :ok
+
+    click_link "Sign up now!"
+    fill_in "Name", with: 'taro'
+    fill_in "Email", with: 'foo@example.com'
+    fill_in "Password", with: 'test'
+    fill_in "Confirmation", with: 'test'
+    click_on 'Create my account'
+
+    # expect(page).to have_content "アカウント登録が完了しました。"
+  end
+end
