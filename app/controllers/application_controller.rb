aeca524fff048a9c40714451a567d0feb84ebd06
zizynonno <zizynonno@gmail.com>
2020-07-27 19:02:16 +0900

    add: micropost search ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
         redirect_to login_url
       end
     end
+
+    # マイクロポストの検索のStrongParameter
+    def microposts_search_params
+      params.require(:q).permit(:content_cont)
+    end
 end
